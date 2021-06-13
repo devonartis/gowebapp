1f9f3a8c3f4067a4de05267224901367a93d41b2
Devon Artis <562122-devonartis@users.noreply.gitlab.com>
2021-06-13 16:15:50 -0400

    Used Julien router - Do Not Merge

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/go-chi/chi/v5 v5.0.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,43 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	
-	"github.com/go-chi/chi/v5"
-	_ "github.com/go-chi/chi/v5/middleware"
 
+	"github.com/julienschmidt/httprouter"
 )
 
 var h http.Handler
 
-func faq(w http.ResponseWriter, http *http.Request) {
+func faq(w http.ResponseWriter, http *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>FAQ Placeholder!</h1>")
 }
-func home(w http.ResponseWriter, http *http.Request) {
+func home(w http.ResponseWriter, http *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to My Simple Golang Site!</h1>")
 }
 
-func contact(w http.ResponseWriter, http *http.Request) {
+func contact(w http.ResponseWriter, http *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch please send an email "+
 		"to <a href=\"mailto:support@devonartis.com\">"+"devon@devonartis.com</a>.")
 }
 
-func custom404(w http.ResponseWriter, http *http.Request) {
+func custom404(w http.ResponseWriter, http *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>404 Error</h1>"+"<p>The page your are looking for does not exist."+
 		"</p><p><b>Please email support</b></p>")
 }
+
 func main() {
-	/**
-	h = http.HandlerFunc(custom404)
-	r.NotFoundHandler = h
-	**/
-	r := chi.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	http.ListenAndServe(":3000", r)
+
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
